1a73037b6703c0f01a301853ecff8a96141b9e18
admin <admin@Mac-04.local>
2014-01-12 09:59:49 +0800

    Initial Commit

---
+++ HelloWorld/DemoViewController.m
@@ -0,0 +1,29 @@
+//
+//  DemoViewController.m
+//  HelloWorld
+//
+//  Created by admin on 14/1/12.
+//  Copyright (c) 2014年 LDF. All rights reserved.
+//
+
+#import "DemoViewController.h"
+
+@interface DemoViewController ()
+
+@end
+
+@implementation DemoViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ HelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by admin on 14/1/12.
+//  Copyright (c) 2014年 LDF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DemoAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DemoAppDelegate class]));
+    }
+}
